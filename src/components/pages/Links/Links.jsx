5750e36ab24af16ce6b262c20c174397240724d1
shivamshaw07 <shivamshaw9005@gmail.com>
2023-11-18 17:56:33 +0530

    made responsive

--- src/components/pages/Links/Links.jsx
+++ src/components/pages/Links/Links.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import {RxLinkedinLogo} from 'react-icons/rx'
 import {BsGithub} from 'react-icons/bs'
-import {FiTwitter} from 'react-icons/fi'
+import {FiTwitter,FiMail} from 'react-icons/fi'
 import {BsInstagram} from 'react-icons/bs'
-import {GrMailOption} from 'react-icons/gr'
 
 const Links = () => {
   return (
-    <ul className='flex gap-2'>
-      <li className='bg-[#3882cd] rounded-full'><a href="https://www.linkedin.com/in/shivam-shaw-1b3690252/"><RxLinkedinLogo className=' h-[30px] w-[30px] rounded-full '/></a></li>
-      <li className='bg-[#3882cd] rounded-full'><a href="https://github.com/shivamshaw07"><BsGithub className=' h-[30px] w-[30px] rounded-full '/></a></li>
-      <li className='bg-[#3882cd] rounded-full'><a href="https://twitter.com/shivamshaw007"></a><FiTwitter className=' h-[30px] w-[30px] rounded-full '/></li>
-      <li className='bg-[#3882cd] rounded-full'><a href="https://www.instagram.com/_shivam.js/"></a><BsInstagram className=' h-[30px] w-[30px] rounded-full '/></li>
-      <li className='bg-[#3882cd] rounded-full'><a href="mailto:shivamshaw9005@gmail.com"></a><GrMailOption className=' h-[30px] w-[30px] rounded-full '/></li>
+    <ul className='flex justify-center items-center gap-2'>
+      <li className='text-[#3882cd] p-1 rounded-full'><a href="https://www.linkedin.com/in/shivam-shaw-1b3690252/"><RxLinkedinLogo className=' h-[30px] w-[30px]'/></a></li>
+      <li className='text-[#3882cd] p-1 rounded-full'><a href="https://github.com/shivamshaw07"><BsGithub className=' h-[30px] w-[30px]'/></a></li>
+      <li className='text-[#3882cd] p-1 rounded-full'><a href="https://twitter.com/shivamshaw007"><FiTwitter className=' h-[30px] w-[30px]'/></a></li>
+      <li className='text-[#3882cd] p-1 rounded-full'><a href="https://www.instagram.com/_shivam.js/"><BsInstagram className=' h-[30px] w-[30px] '/></a></li>
+      <li className='text-[#3882cd] p-1 rounded-full'><a href="mailto:shivamshaw9005@gmail.com"><FiMail className='text-[#3882cd] h-[33px] w-auto'/></a></li>
     </ul>
   )
 }
