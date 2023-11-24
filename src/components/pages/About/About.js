4be7431d4efb03d1866a6c4753576e62435d8f16
shivamshaw07 <shivamshaw9005@gmail.com>
2023-11-24 13:59:57 +0530

    added links

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -99,7 +99,7 @@ const About = () => {
       </div>
       <a href={pic} download={pic}>
       <button className="px-8 py-3 bg-[#4b97dd] rounded-full text-xl font-semibold hover:font-bold hover:scale-90 shadow-xl hover:shadow-2xl transition-all duration-500">
-        Resume <span><FaChevronCircleRight className="inline "/></span>
+        <a href="https://drive.google.com/drive/u/0/folders/1tbq80283zFXK_w2z1cj_c6yXrukAaHrf">Resume</a> <span><FaChevronCircleRight className="inline "/></span>
       </button>
       </a>
     </div>
