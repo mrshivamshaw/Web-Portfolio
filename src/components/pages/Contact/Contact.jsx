abffdd6a0e51c199712f28c59366dc6c7de81d11
shivamshaw07 <shivamshaw9005@gmail.com>
2023-12-28 23:22:13 +0530

    skill

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -43,17 +43,17 @@ const Contact = () => {
             Qui, ipsam.
           </div>
           <div>
-            <div className="text-white/40 flex justify-start items-center gap-[6vw] border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
+            <div className="text-white/40 flex justify-start items-center  border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
               <div className="">ADDRESS</div>
-              <div className="text-white/90"> : DHANBAD, JHARKHAND</div>
+              <div className="text-white/90"> :&nbsp;&nbsp; DHANBAD, JHARKHAND</div>
             </div>
-            <div className="text-white/40 flex justify-start items-center gap-[8vw] border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
+            <div className="text-white/40 flex justify-start items-center  border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
               <div className="">PHONE</div>
-              <div className="text-white/90"> : +91 9142574541</div>
+              <div className="text-white/90"> :&nbsp;&nbsp; +91 9142574541</div>
             </div>
-            <div className="text-white/40 flex justify-start gap-[3vw] items-center border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
+            <div className="text-white/40 flex justify-start  items-center border-b border-white/40 w-full pb-2 xl:text-lg lg:text-lg md:text-lg text-base">
               <div className="">EMAIL</div>
-              <div className="text-white/90"> : shivamshaw9005@gmail.com</div>
+              <div className="text-white/90"> :&nbsp;&nbsp; shivamshaw9005@gmail.com</div>
             </div>
           </div>
           <Links />
