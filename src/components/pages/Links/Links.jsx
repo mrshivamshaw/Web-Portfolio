442578da251430828b69199c8a9975008e517546
SHIVAM KUMAR SHAW <108209696+mrshivamshaw@users.noreply.github.com>
2024-09-16 17:34:11 +0530

    Update Links.jsx

--- src/components/pages/Links/Links.jsx
+++ src/components/pages/Links/Links.jsx
@@ -10,7 +10,7 @@ const Links = () => {
       <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40'><a  target="_blank" href="https://www.linkedin.com/in/shivam-shaw-1b3690252/"><RxLinkedinLogo className=' h-[25px] text-white w-auto'/></a></li>
       <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40' ><a target="_blank" href="https://github.com/mrshivamshaw"><BsGithub className=' h-[25px] w-auto text-white'/></a></li>
       <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40' ><a target="_blank" href="https://twitter.com/mrshivamshaw"><FiTwitter className=' h-[25px] w-auto text-white'/></a></li>
-      <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40'><a  target="_blank" href="https://www.instagram.com/_shivam.js/"><BsInstagram className=' h-[25px] w-auto text-white'/></a></li>
+      <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40'><a  target="_blank" href="https://www.instagram.com/_mrshivamshaw/"><BsInstagram className=' h-[25px] w-auto text-white'/></a></li>
       <li className='p-2 rounded-md border-[1px] border-black/20 bg-white/10 hover:bg-black/40'><a  target="_blank" href="mailto:shivamshaw9005@gmail.com"><FiMail className='h-[25px] w-auto text-white'/></a></li>
     </ul>
   )
